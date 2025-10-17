5dca3f5f899b9efa659baff2f0139852d09f6028
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2025-10-17 20:26:29 -0300

    feat: infos reais na pagina account

--- src/app/home/account/page.tsx
+++ src/app/home/account/page.tsx
@@ -1,16 +1,22 @@
 import { ToggleDarkMode } from "@/shared/components/toggle-dark-mode";
 import { ToggleLayoutMode } from "@/shared/components/toggle-layout-mode";
 import { BtnLogout } from "./components/btn-logout";
+import { auth } from "@/shared/libs/better-auth/auth";
+import { headers } from "next/headers";
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
   return (
     <>
       <div className="card">
         <div className="flex justify-between items-center">
           <div>
-            <h2 className="text-lg font-semibold">Rafael M</h2>
-            <p className="text-sm text-gray-600 dark:text-gray-300">
-              rafaelmeloalvessouza@gmail.com
+            <h2 className="text-lg font-semibold">{session?.user?.name}</h2>
+            <p className="text-sm text-gray-600 dark:text-gray-300 decoration-0">
+              {session?.user?.email}
             </p>
           </div>
           <BtnLogout />
@@ -21,7 +27,6 @@ export default function AccountPage() {
           <ToggleLayoutMode />
         </div>
       </div>
-
     </>
   );
 }
