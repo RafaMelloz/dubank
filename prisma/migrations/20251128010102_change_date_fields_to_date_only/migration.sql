4dac02b33ae92477c6f82333731f26fdc7ec5ee9
RafaMelloz <rafaelmeloalvessouza@gmail.com>
2025-11-28 10:27:59 -0300

    alterando tipo de campo date

---
+++ prisma/migrations/20251128010102_change_date_fields_to_date_only/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "expense" ALTER COLUMN "date" SET DATA TYPE DATE;
+
+-- AlterTable
+ALTER TABLE "income" ALTER COLUMN "date" SET DATA TYPE DATE;
+
+-- AlterTable
+ALTER TABLE "monthly_registers" ALTER COLUMN "registerDate" SET DATA TYPE DATE;
